2da8ae7714cff7fe21201c53c38420d54cdcd237
Duong Trung Thanh <thanhdt3019@gmail.com>
2025-10-24 13:40:25 +0700

    first commit

---
+++ src/main/java/init/upinmcse/cctvcore/CctvCoreApplication.java
@@ -0,0 +1,13 @@
+package init.upinmcse.cctvcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctvCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CctvCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/init/upinmcse/cctvcore/CctvCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package init.upinmcse.cctvcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctvCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
