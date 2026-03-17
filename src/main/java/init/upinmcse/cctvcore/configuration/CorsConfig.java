c78f01128adca75d5c6f0f5c86e252a0da1ac626
Duong Trung Thanh <thanh.dt@eastgate-software.com>
2026-03-17 13:41:38 +0700

    Feat: develop module auth

---
+++ src/main/java/init/upinmcse/cctvcore/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package init.upinmcse.cctvcore.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
